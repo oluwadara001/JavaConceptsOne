38c221726429fb3ab83aa5479b5cc2a3ca4cf970
oluwadara001 <40069286+oluwadara001@users.noreply.github.com>
2019-06-17 14:55:19 -0500

    The 2 methods works lamely. Need to re-write them by leveraging the return value of one method in another. "double salaray" in this case.

--- JavaConceptsOne/src/com/YomiOluwadara/Abstraction/ObjectsAndMethodsPract1.java
+++ JavaConceptsOne/src/com/YomiOluwadara/Abstraction/ObjectsAndMethodsPract1.java
@@ -7,29 +7,43 @@ import java.util.Scanner;
 3 - 'AddWork()' which adds $5 to salary of employee if the number of hours of work per day is more than 6 hours
 4 - final() -prints the final salary
 other option: list all the variable and use getters and setter methods
+FURTHER WORK: re-write the methods so the salary returned from the getSalaryInfo( ) could be used as a variable in other methods
+	--figure out how to implement
  */
 public class ObjectsAndMethodsPract1 {
-	//getting user i/p from keyboard
 	
+	//getting user i/p from keyboard
 	 double getSalaryInfo( ) {
 		System.out.println("Enter Employee salary");
 		Scanner scan = new Scanner (System.in);
 		double salary = scan.nextDouble();
+		if(salary <= 500) {
+			salary = salary + 10;
+			}
+		else {
+				salary = salary;
+		}
 		return salary;
 		}
-	double getNoOfHoursWorked() {
+		
+	double hourBasedSalary() {
+		double SalarybasedOnHour = 0;
 		System.out.println("Enter Employee number of worked per day");
 		Scanner scan2 = new Scanner(System.in);
 		double noOfHours = scan2.nextDouble();
-		return noOfHours;
+		if(noOfHours > 6) {
+		SalarybasedOnHour = getSalaryInfo() + 5;
+				}
+		return SalarybasedOnHour;
 		}
-
-	public static void main(String[] args) {
-		//crate and object of the class ObjectsAndMethodsPract1
-		ObjectsAndMethodsPract1 Gena = new ObjectsAndMethodsPract1 ();
-		System.out.println(Gena.getNoOfHoursWorked());
-		System.out.println(Gena.getSalaryInfo());
 	
+	public static void main(String[] args) {
+		
+		ObjectsAndMethodsPract1 Gena = new ObjectsAndMethodsPract1 ();
+		//System.out.println(Gena.getSalaryInfo());
+		System.out.println(Gena.hourBasedSalary());
+		
+		
 	}
 
 }

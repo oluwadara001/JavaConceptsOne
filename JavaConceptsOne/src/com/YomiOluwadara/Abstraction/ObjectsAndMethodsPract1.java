4ca6108855f280611b1716aa003c86948b26a5b3
oluwadara001 <40069286+oluwadara001@users.noreply.github.com>
2019-06-17 08:48:32 -0500

    First exercise on objects and methods

---
+++ JavaConceptsOne/src/com/YomiOluwadara/Abstraction/ObjectsAndMethodsPract1.java
@@ -0,0 +1,22 @@
+package com.YomiOluwadara.Abstraction;
+/*
+ * Write a program by creating an 'Employee' class having the following methods and print the final salary.
+1 - 'getInfo()' which takes the salary, number of hours of work per day of employee as parameter
+2 - 'AddSal()' which adds $10 to salary of the employee if it is less than $500.
+3 - 'AddWork()' which adds $5 to salary of employee if the number of hours of work per day is more than 6 hours
+4 - final() -prints the final salary
+ */
+public class ObjectsAndMethodsPract1 {
+	//declare non static- variables
+	double salary;
+	double noOfHoursWorkedPerDay;
+	void getInfo( double salary, double noOfHoursWorkedPerDay) {
+		
+	}
+	
+
+	public static void main(String[] args) {
+
+	}
+
+}

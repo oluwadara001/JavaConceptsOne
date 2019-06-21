cf60e122582fa10a9032869550096cb468cbacf2
oluwadara001 <40069286+oluwadara001@users.noreply.github.com>
2019-06-21 09:50:46 -0500

    added more duplicate entries to the has set to see if they print

--- JavaConceptsOne/src/com/YomiOluwadara/HashSetAndHashMap/HashSetDeclartion.java
+++ JavaConceptsOne/src/com/YomiOluwadara/HashSetAndHashMap/HashSetDeclartion.java
@@ -13,9 +13,14 @@ public class HashSetDeclartion {
 		schoolNumberofStudents.add(9987);
 		schoolNumberofStudents.add(4563);
 		schoolNumberofStudents.add(9632);
+		schoolNumberofStudents.add(9632);
+		schoolNumberofStudents.add(9632);
+		schoolNumberofStudents.add(9632);
 		//print the content of the integer type hash set
-		System.out.println(schoolNumberofStudents);
+		System.out.println("The distinct  elements of the integer type Hashset is  " + schoolNumberofStudents);
+		//more operation that can be invoked on Hash set by using the reference to invoke the applicable methods in the java built in library
 		
+	
 		//Set <Integer> schoolNumberofStudentsEnrolledIn89 = new HashSet<Integer>();
 		
 		

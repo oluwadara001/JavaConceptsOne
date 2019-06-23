7c61c75fd627a722986f26665013252c563a0188
oluwadara001 <40069286+oluwadara001@users.noreply.github.com>
2019-06-23 15:45:38 -0500

    HashMap and HashSet declaration

--- JavaConceptsOne/src/com/YomiOluwadara/HashSetAndHashMap/HashSetDeclartion.java
+++ JavaConceptsOne/src/com/YomiOluwadara/HashSetAndHashMap/HashSetDeclartion.java
@@ -19,15 +19,23 @@ public class HashSetDeclartion {
 		//print the content of the integer type hash set
 		System.out.println("The distinct  elements of the integer type Hashset is  " + schoolNumberofStudents);
 		//more operation that can be invoked on Hash set by using the reference to invoke the applicable methods in the java built in library
+		System.out.println("The size/cardinality/count distinct of elements in this hash set is "  + schoolNumberofStudents.size());
+		System.out.println("Is this has set empty? " + schoolNumberofStudents.isEmpty());
+		//declaring a has ste of type String
+		Set <String> ListOfHowToTakeMed = new HashSet<String>();
+		ListOfHowToTakeMed.add("bd- take at bed time");
+		ListOfHowToTakeMed.add("td- once a day");
+		ListOfHowToTakeMed.add("tid-three times a day");
+		ListOfHowToTakeMed.add("qid- three times a day");
+		ListOfHowToTakeMed.add("qy- take e=one every h hour");
+		//iterate over the String hash set and print the content
+		for(String name:ListOfHowToTakeMed) {
+			System.out.println(name);
+			
+		}
 		
-	
-		//Set <Integer> schoolNumberofStudentsEnrolledIn89 = new HashSet<Integer>();
 		
-		
-		
-		
-
-	}
+		}
 
 }
 

421891f78dfa2182efc65115e92f91fe23ea3471
oluwadara001 <40069286+oluwadara001@users.noreply.github.com>
2019-06-26 20:40:59 -0500

    overloaded constructors so when objects can initialized differently.

--- JavaConceptsOne/src/com/YomiOluwadara/DateClass/DateDeclaration.java
+++ JavaConceptsOne/src/com/YomiOluwadara/DateClass/DateDeclaration.java
@@ -1,3 +1,6 @@
+/*
+ * practising custome type of date and using a date instance to invoke available methods in the date class
+ */
 package com.YomiOluwadara.DateClass;
 
 import java.util.Date;
@@ -5,18 +8,42 @@ import java.text.SimpleDateFormat;
 import java.util.*; // always import all to avoid issues 
 
 public class DateDeclaration {
-	//declaring a variable of type Date ( we can do that for imported classes and arrays)
- Date myDate ;
-	public Date getMyDate() {
-	return myDate;
-}
-public void setMyDate(Date myDate) {
-	this.myDate = myDate;
-}
+//declaring a variable of type Date ( we can do that for imported classes and arrays)
+ Date medicationStartDate ;
+ Date medicationEndDate;
+ 
+ 
+ // defining setters and getters for the variables
+ public void setMedicationStartDate(Date medicationStartDate) {
+	 this.medicationStartDate = medicationStartDate;
+ }
+ public void setMedicationEndDate(Date medicationEndDate) {
+	 this.medicationEndDate = medicationEndDate;
+ }
+ public Date getMedicationStartDate() {     // notice return type is of "Date" 
+	 return medicationStartDate;
+ }
+ 
+ public Date getMedicationEndDate() {
+	 return medicationEndDate;
+ }
 
+
+ 
+//constructors - they will be overloaded so that I can be able to leverage them when I want the instance/object to use the setters and getters to get values
+ void DateDeclaration () {
+	 
+ }
+//constructor 2 
+ void DateDeclaration (Date medicationStartDate, Date medicationEndDate) {
+	 this.medicationStartDate = medicationStartDate;
+	 this.medicationEndDate = medicationEndDate;
+ }
+ 
 	public static void main(String[] args) {
+		
+	
 		Date medStartDate = new Date();
-		//use date object to invoke availabe methods in the date class
 		System.out.println(medStartDate.toString());
 
 		

02cfc87f5ffa2b175dc88e71192c85e71874843a
Yomi Oluwadara <40069286+oluwadara001@users.noreply.github.com>
2020-03-11 10:28:29 -0500

    added package for java i/o- reading files

---
+++ src/com/YomiOluwadara/ReadingFiles/ReadFileByScannerAndSaveFileToAlocation.java
@@ -0,0 +1,31 @@
+package com.YomiOluwadara.ReadingFiles;
+/*Notes:
+ *  @author OO046152 : Yomi Oluwadara
+ * 
+ * read file from one location and save it to a different location
+ * 
+ * 
+ *         Use an object of the file class to specify the location of the file
+ *         to be read. Use an object of the scanner class to invoke the
+ *         appropriate scanner class method that read the file line by line
+ *         
+ *         Declare a String variable to hold the new file
+ *        	use the  concat() to concat each line of the file together + "\n", then use
+ *        		each line is read using the hasNextLine()
+ *         use an Object of the FileWritter class to write to a specified location to save file
+ *         use an Object of the FileWritter class to invoke the write()
+ *         use an Object of the FileWritter class to invoke the close()
+ *          
+ */
+
+public class ReadFileByScannerAndSaveFileToAlocation {
+
+	public void readFileAndWriteIt() {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}

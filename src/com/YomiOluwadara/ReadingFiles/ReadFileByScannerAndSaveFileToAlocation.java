8660bdcbabb9f0f1a3b13aab4ed36ed6363d6a9e
Yomi Oluwadara <40069286+oluwadara001@users.noreply.github.com>
2020-03-11 14:53:39 -0500

    implemented logic to read i/p text file and write it to a specified directory

--- src/com/YomiOluwadara/ReadingFiles/ReadFileByScannerAndSaveFileToAlocation.java
+++ src/com/YomiOluwadara/ReadingFiles/ReadFileByScannerAndSaveFileToAlocation.java
@@ -1,4 +1,11 @@
 package com.YomiOluwadara.ReadingFiles;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 /*Notes:
  *  @author OO046152 : Yomi Oluwadara
  * 
@@ -21,11 +28,31 @@ package com.YomiOluwadara.ReadingFiles;
 public class ReadFileByScannerAndSaveFileToAlocation {
 
 	public void readFileAndWriteIt() {
+		File file = new File("C:\\Users\\oo046152\\mytestfile.txt");
+		try {
+			Scanner scanner = new Scanner(file);
+			String newFile = "";
+			while (scanner.hasNextLine()) {
+				// storing the content of input file to a file that will be exported
+				newFile = newFile.concat(scanner.nextLine() + "\n");
+				// Using the FileWriter object to specify location,write and close file
+				FileWriter fileWriter = new FileWriter("C:\\Users\\oo046152\\Desktop\\fileFromJava.txt");
+				fileWriter.write(newFile);
+				fileWriter.close();
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File to be read cannot be found");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Could you not to file");
+			e.printStackTrace();
+		}
 
 	}
 
 	public static void main(String[] args) {
-
+		ReadFileByScannerAndSaveFileToAlocation obj = new ReadFileByScannerAndSaveFileToAlocation();
+		obj.readFileAndWriteIt();
 	}
 
 }

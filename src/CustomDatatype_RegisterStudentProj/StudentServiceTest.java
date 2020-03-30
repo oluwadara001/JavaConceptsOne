299b7ad9aa2bfccd50cbf698e849c311531e4b1d
Yomi Oluwadara <40069286+oluwadara001@users.noreply.github.com>
2020-03-30 14:39:52 -0500

    writing unit test for the RegisterStudent proj

---
+++ src/CustomDatatype_RegisterStudentProj/StudentServiceTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package CustomDatatype_RegisterStudentProj;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author OO046152 : Yomi Oluwadara
+ *
+ */
+class StudentServiceTest {
+
+	@DisplayName("collects keyboard info relating to student and calls printStudentInfo() that prints it ")
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
